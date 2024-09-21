d4062505b13ad7f9f8e0ac444e7f3df704cc3d86
anishjangra <anishjangra.dev@gmail.com>
2024-09-21 12:35:21 +0530

    padding

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,15 +1,12 @@
-import React from 'react';
 import Container from '@mui/material/Container';
 import MainContent from './MainContent';
 import Latest from './Latest';
 
 export default function Blog() {
   return (
-        <Container         
-        maxWidth="lg"
-        component="main">
-          <MainContent />
-          <Latest title='Latest Sessions' courseId={null}/>
-        </Container>
+    <Container maxWidth="lg" component="main">
+      <MainContent />
+      <Latest title="Latest Sessions" style={{ paddingTop: 16 }} />
+    </Container>
   );
 }
